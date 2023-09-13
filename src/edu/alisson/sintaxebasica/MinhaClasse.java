2dcfcd46a69981c661ac19a0e98e1bc23a8daa53
Alisson Rego <alissondroid07@gmail.com>
2023-09-13 08:57:15 -0300

    commit inicial

---
+++ src/edu/alisson/sintaxebasica/MinhaClasse.java
@@ -0,0 +1,14 @@
+package edu.alisson.sintaxebasica;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        int numeroUm = 2;
+        int numeroDois = 6;
+
+        System.out.println(somar(numeroUm, numeroDois));
+    }
+
+    public static int somar(int num1, int num2) {
+        return num1 + num2;
+    }
+}
